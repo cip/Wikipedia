450412e3e10f9d9a28ba8e34471f35098e3bf89b
cip <cip@gmx.at>
2012-02-25 22:52:06 +0100

    Qt: Replaced menu from web platform.js with menu from ios platform.js
    Icons now displayed (perhaps bug in original web platform.js, as not
    working in chrome neither).
    Note that popup menus (4th and 5th) button stopped working due to this change

--- assets/www/qt/platform.js
+++ assets/www/qt/platform.js
@@ -13,83 +13,103 @@ chrome.addPlatformInitializer(function() {
         $('html').removeClass('goodscroll').addClass('badscroll');
 });
 
-function updateMenuState() {
-	var items = [
-		{
-			id: 'menu-back',
-			action: chrome.goBack
-		},
-		{
-			id: 'menu-forward',
-			action: chrome.goForward
-		},
-		{
-			id: 'menu-language',
-			action:  languageLinks.showAvailableLanguages
-		},
-		{
-			id: 'menu-output',
-			action: function() {
-				popupMenu([
-					mw.msg('menu-savePage'),
-					mw.msg('menu-sharePage'),
-					mw.msg('menu-cancel')
-				], function(value, index) {
-					if (index == 0) {
-						savedPages.saveCurrentPage();
-					} else if (index == 1) {
-						sharePage();
-					}
-				}, {
-					cancelButtonIndex: 2,
-					origin: this
-				});
-			}
-		},
-		{
-			id: 'menu-sources',
-			action: function() {
-				popupMenu([
-					mw.msg('menu-nearby'),
-					mw.msg('menu-savedPages'),
-					mw.msg('menu-history'),
-					mw.msg('menu-cancel')
-				], function(val, index) {
-					if (index == 0) {
-						geo.showNearbyArticles();
-					} else if (index == 1) {
-						savedPages.showSavedPages();
-					} else if (index == 2) {
-						appHistory.showHistory();
-					}
-				}, {
-					cancelButtonIndex: 3,
-					origin: this
-				});
-			}
-		},
-		{
-			id: 'menu-settings',
-			action: appSettings.showSettings
-		}
-	];
-	$('#menu').remove();
-	var $menu = $('<div>');
-	$menu
-		.attr('id', 'menu')
-		.appendTo('body');
+function setMenuItemState(action, state) {
+    // Stupid iterator
+    $.each(menu_items, function(i, item) {
+        if(item.id == action) {
+            item.disabled = !state;
+        }
+    });
+    updateMenuState();
+}
 
-	$.each(items, function(i, item) {
-		var $button = $('<button>');
-		$button
-			.attr('id', item.id)
-			.attr('title', mw.msg(item.id))
-			.click(function() {
-				item.action.apply(this);
-			})
-			.append('<span>')
-			.appendTo($menu);
-	});
+function setPageActionsState(state) {
+    setMenuItemState("page-actions", state);
+}
+
+var menu_items = [
+    {
+        id: 'go-back',
+        action: chrome.goBack
+    },
+    {
+        id: 'go-forward',
+        action: chrome.goForward,
+        disabled: true
+    },
+    {
+        id: 'read-in',
+        action:  languageLinks.showAvailableLanguages
+    },
+    {
+        id: 'page-actions',
+        action: function() {
+            popupMenu([
+                mw.msg('menu-savePage'),
+                mw.msg('menu-sharePage'),
+                mw.msg('menu-cancel')
+            ], function(value, index) {
+                if (index == 0) {
+                    savedPages.saveCurrentPage();
+                } else if (index == 1) {
+                    sharePage();
+                }
+            }, {
+                cancelButtonIndex: 2,
+                origin: this
+            });
+        }
+    },
+    {
+        id: 'list-actions',
+        action: function() {
+            popupMenu([
+                mw.msg('menu-nearby'),
+                mw.msg('menu-savedPages'),
+                mw.msg('menu-history'),
+                mw.msg('menu-cancel')
+            ], function(val, index) {
+                if (index == 0) {
+                    geo.showNearbyArticles();
+                } else if (index == 1) {
+                    savedPages.showSavedPages();
+                } else if (index == 2) {
+                    appHistory.showHistory();
+                }
+            }, {
+                cancelButtonIndex: 3,
+                origin: this
+            });
+        }
+    },
+    {
+        id: 'view-settings',
+        action: appSettings.showSettings
+    }
+];
+
+function updateMenuState() {
+    $('#menu').remove();
+    var $menu = $('<div>');
+    $menu
+        .attr('id', 'menu')
+        .appendTo('body');
+
+    $.each(menu_items, function(i, item) {
+        var $button = $('<button>');
+        $button
+            .attr('id', item.id)
+            .attr('title', mw.msg(item.id));
+        if(item.disabled) {
+            $button.addClass("disabled");
+        } else {
+            $button.click(function() {
+                item.action.apply(this);
+            });
+        }
+        $button.append('<span>')
+            .appendTo($menu);
+    });
 };
 
 // @Override
@@ -97,26 +117,16 @@ function getPhoneGapVersion(callback, error) {
 	callback('n/a');
 }
 
+
 // @Override
 function popupMenu(items, callback, options) {
-	options = $.extend({destructiveButtonIndex: null, cancelButtonIndex: null}, options || {});
-
-	var $bg = $('<div class="actionsheet-bg"></div>').appendTo('body'),
-		$sheet = $('<div class="actionsheet"></div>').appendTo('body');
-	$.each(items, function(index, label) {
-		var $button = $('<button>')
-			.text(label)
-			.appendTo($sheet)
-			.click(function() {
-				$sheet.remove();
-				$bg.remove();
-				callback(label, index);
-			});
-		if (index === options.destructiveButtonIndex) {
-			$button.addClass('destructive');
-		}
-		if (index === options.cancelButtonIndex) {
-			$button.addClass('cancel');
-		}
-	});
+    if (options.origin) {
+        var $origin = $(options.origin),
+            pos = $origin.offset();
+        options.left = pos.left;
+        options.top = 0; // hack pos.top;
+        options.width = $origin.width();
+        options.height = $origin.height();
+    }
+    window.plugins.actionSheet.create('', items, callback, options);
 }
